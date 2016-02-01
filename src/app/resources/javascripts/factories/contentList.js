b7209f73018fa1fc7611b6ce1a53c65ac4e5221b
Valentina Grajales <valegrajales@gmail.com>
2016-02-01 15:20:32 -0500

    Fix with image size in firefox browser

--- src/app/resources/javascripts/factories/contentList.js
+++ src/app/resources/javascripts/factories/contentList.js
@@ -6,5 +6,5 @@ angular.module('catalogHome')
 	// Acces to catalog of biodiversity api
 	// =========================================================================
 	.factory('SpecieFactory', ['$resource', function($resource){
-		return $resource("http://localhost:4000/api/fichas/search", {q:'@q', pagesize:'@pagesize', page:'@page', order:'@order', sort:'@sort'});
+		return $resource("http://192.168.205.196:4000/api/fichas/search", {q:'@q', pagesize:'@pagesize', page:'@page', order:'@order', sort:'@sort'});
 	}]);
